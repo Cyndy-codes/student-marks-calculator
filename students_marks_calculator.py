397d8f301c037fe7698193b4d20ea2de6f2dd2ac
cyndy <cyndy@gmail.com>
2026-09-10 14:04:53 -0700

    add student_marks_calculator

---
+++ students_marks_calculator.py
@@ -0,0 +1,26 @@
+name=input("enter your name:")
+math=int(input("enter math mark:"))
+english=int(input("enter english mark:"))
+networking=int(input("enter networking mark:"))
+database=int(input("enter database mark:"))
+computerstudies=int(input("enter computer studies mark"))
+
+total=math+english+networking+database+computerstudies
+average=total/5
+if average >=80:
+    grade="A"
+elif average >=70:
+    grade="B"
+elif average >=60:
+    grade="C"
+elif average >=50:
+    grade="D"
+
+else:
+    grade="E"
+
+print("\n----STUDENTS PROJECT----")
+print("student:", name)
+print("total:", total)
+print("average:", average)
+print("Grade:", grade)
